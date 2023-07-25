c4a5aa7329a6ee2017ec990d2226ff749e168679
João Gordox <jpgertto.120fernadesz@gmail.com>
2023-07-25 09:17:31 -0300

    Criado o formulário de login juntamente com sua estilização

--- public/login.php
+++ public/login.php
@@ -6,7 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cube Place</title>
     <link rel="stylesheet" href=".././Styles/header.css">
+    <link rel="stylesheet" href=".././Styles/form.css">
 </head>
 <body>
+    <form action="" method="post">
+        <label for="email_user">Email: </label>
+        <input type="email" name="email_user" id="email_user" class="input-form">
+
+        <label for="pwd_user">Senha: </label>
+        <input type="password" name="pwd_user" id="pwd_user" class="input-form">
+    
+        <button type="submit">Entrar</button>
+    </form>
+
 </body>
 </html>
