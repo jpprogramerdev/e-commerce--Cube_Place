ee7751dcfb047201e5a3ab736cccb6c770d344b0
João Gordox <jpgertto.120fernadesz@gmail.com>
2023-07-25 08:56:45 -0300

    Criado arquivo de login

--- public/login.php
+++ public/login.php
@@ -8,51 +8,5 @@
     <link rel="stylesheet" href=".././Styles/header.css">
 </head>
 <body>
-    <header class="header">
-        <a href="index.html"><img src=".././Images/logo-example.png" alt="Logo"></a>
-        <nav class="menu-header">
-            <ul>
-                <li class="dropdown"><a href="">Todos</a></li>
-                <li class="dropdown">
-                    <a href="#">Cubos</a>
-                    <ul class="dropdown-content">
-                        <li><a href="">2x2x2</a></li>
-                        <li><a href="">3x3x3</a></li>
-                        <li><a href="">4x4x4</a></li>
-                        <li><a href="">5x5x5</a></li>
-                        <li><a href="">6x6x6</a></li>
-                        <li><a href="">7x7x7</a></li>
-                    </ul>
-                </li>
-
-                <li class="dropdown">
-                    <a href="#">Outros</a>
-                    <ul class="dropdown-content">
-                        <li><a href="">Skewb</a></li>
-                        <li><a href="">Pyraminx</a></li>
-                        <li><a href="">Square-1</a></li>
-                        <li><a href="">Megaminx</a></li>
-                        <li><a href="">Clock</a></li>
-                    </ul>
-                </li>
-
-                <li class="dropdown">
-                    <a href="#">Acessórios</a>
-                    <ul class="dropdown-content">
-                        <li><a href="">Lubrificantes</a></li>
-                        <li><a href="">Suportes</a></li>
-                        <li><a href="">Bolsa</a></li>
-                        <li><a href="">Megaminx</a></li>
-                        <li><a href="">Timer</a></li>
-                        <li><a href="">Tapes</a></li>
-                        <li><a href="">Cover</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown"><a href="./public/login.php">Login</a></li>
-            </ul>
-        </nav>
-    </header>
-
-    <form action="" method="post"></form>
 </body>
 </html>
